563b67b5080a96f2bf2ace1c63fdac249b7d6be5
David Jonathan <davejona@yahoo.com>
2020-12-25 12:04:51 +0100

    update

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,19 @@
 import React from "react";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+
+import { Grid, Slider } from "@material-ui/core";
 import "./Footer.css";
+
+
+
 
 function Footer() {
     return (
@@ -9,7 +23,11 @@ function Footer() {
         </div>
 
         <div className="footer__center">
-        <p>Player controls</p>
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon"/>
+        <PlayCircleOutlineIcon fontSize="large" className="footer__icon"/>
+        <SkipNextIcon className="footer__icon"/>
+        <RepeatIcon className="footer__green"/>
         </div> 
 
         <div className="footer__right">
